1f4b15334f113e2aa3cb7e773a081a92c9dcf2a1
Jeremy Leonardo <58095255+jeremy-leonardo@users.noreply.github.com>
2021-03-19 08:02:50 +0000

    add reconnection attempt on redis connection error

--- subscriber/app.py
+++ subscriber/app.py
@@ -11,12 +11,18 @@ def main():
     r = redis.Redis(host=REDIS_HOST, decode_responses=True)
     p = r.pubsub(ignore_subscribe_messages=True)
     p.subscribe(CHANNEL)
-
     while True:
-        message = p.get_message()
+        try:
+            message = p.get_message()
+        except redis.ConnectionError:
+            # Do reconnection attempts here such as sleeping and retrying
+            time.sleep(3)
+            p = r.pubsub()
+            p.subscribe(CHANNEL)
         if message:
-            print("Received Message : " + message.get("data", ""))
-        time.sleep(0.001)
+            print(message)
+            print(message['data'])
+            time.sleep(0.001)
 
 
 if __name__ == "__main__":
